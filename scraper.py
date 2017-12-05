8e69c517c556857f31047b4978be0627ffb23a5e
howa <howa.wong@gmail.com>
2017-12-05 13:53:38 +0800

    Initial Commit

--- scraper.py
+++ scraper.py
@@ -22,3 +22,23 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import requests
+from bs4 import BeautifulSoup
+#import scraperwiki
+from datetime import datetime
+import pytz
+req = requests.get('http://www.shunon.com.hk/pricing.php')
+soup = BeautifulSoup(req.content, 'html.parser')
+table = soup.find_all('table', {'class': 'Font16Boldc'})[1]
+element = table.find_all('p')[0]
+text = element.text.split()
+titles = [text[i] for i in (0, 2, 4, 6)]
+values = [int(text[i].replace('$', '').replace(',', ''))
+                  for i in (1, 3, 5, 7)]
+hkt = pytz.timezone('Asia/Hong_Kong')
+dt = datetime.now().replace(tzinfo=hkt).date()
+data = {titles[i]: values[i] for i in range(0, 4)}
+data['date'] = dt
+print(data)
+scraperwiki.sqlite.save(unique_keys=['date'], data=data)
